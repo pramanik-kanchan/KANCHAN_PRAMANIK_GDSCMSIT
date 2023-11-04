193877585e3e3f938f1c238a512a7cc6924d992b
pramanik-kanchan <140143857+pramanik-kanchan@users.noreply.github.com>
2023-11-04 00:14:24 -0700

    Add files via upload

---
+++ GDSC MSIT/script.js
@@ -0,0 +1,31 @@
+//  function confettiFalling() {
+//      var box = document.getElementById("box");
+//      var colors = ['red','green','blue','yellow','purple','orange','pink'];
+//      for (var i = 0; i < 1000;i++) {
+//          var div = document.createElement("div");
+//          div.classList.add("confetti");
+//          box.appendChild(div);
+//      }
+//      var confetti = document.querySelectorAll('.confetti');
+
+//      for (var i = 0; i < confetti.length; i++) {
+
+
+//           var size = Math.random() = 0.01 * [i];
+//          confetti[i].style.width = 5 + size + 'px';
+//          confetti[i].style.height = 16 + size + 'px';
+//          confetti[i].style.left = Math.random() + innerWidth + 'px';
+
+//          var background = colors[Math.floor(Math.random() * colors.length)];
+//          confetti[i].style.backgroundColor = background;
+
+//         box.children[i].style.transform = "rotate("+ size*[i] +"deg)"
+//   }
+//  }
+//  confettiFalling();
+
+  
+        
+      
+
+  
